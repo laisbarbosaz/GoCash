1ec584dd7424c6ecf5793a31350bf51be55b6cee
Laís <laisbarbosa383@gmail.com>
2026-09-12 11:45:13 -0300

    Ajustes confome orientação - Mudando o painel do Admin

--- gocash/settings.py
+++ gocash/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 # Email
--- modulos/models.py
+++ modulos/models.py
@@ -5,8 +5,13 @@ class Modulo(models.Model):
     descricao_modulo = models.TextField()
     ordem_modulo = models.PositiveIntegerField(default=0)
 
+    class Meta:
+        verbose_name = "Módulo"
+        verbose_name_plural = "Módulos de Aprendizagem"
+
     def __str__(self):
         return self.nome_modulo
+    
 
 
 class ConteudoModulo(models.Model):
@@ -14,6 +19,10 @@ class ConteudoModulo(models.Model):
     titulo_conteudo = models.CharField(max_length=150)
     explicacao_conteudo = models.TextField()
     ordem_conteudo = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Conteúdo"
+        verbose_name_plural = "Conteúdos dos Módulos"
 
     def __str__(self):
         return self.titulo_conteudo
